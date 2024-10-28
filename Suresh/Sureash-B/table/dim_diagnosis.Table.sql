efd2b1b4aa763bc2bed3afdb808c034f2a2fabc7
sushenoy <91982218+sushenoy@users.noreply.github.com>
2024-10-27 21:21:30 -0700

    Delete Suresh/Sureash-B/table directory

--- Suresh/Sureash-B/table/dim_diagnosis.Table.sql
+++
@@ -1,8 +0,0 @@
-
-CREATE TABLE purgo_ai_ls.dim_diagnosis (
-	diagnosisid INTEGER, 
-	diagnosisname VARCHAR(100), 
-	diagnosisdescription VARCHAR(256), 
-	icdcode VARCHAR(10)
-)
-
--- Suresh/Sureash-B/table/dim_treatment.Table.sql
+++
@@ -1,8 +0,0 @@
-
-CREATE TABLE purgo_ai_ls.dim_treatment (
-	treatmentid INTEGER, 
-	treatmentname VARCHAR(100), 
-	treatmentdescription VARCHAR(256), 
-	treatmenttype VARCHAR(50)
-)
-
