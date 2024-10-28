abe08772854cbfb8f64e5dbc4b2d89553a6df905
Purgo AI <support@purgo.ai>
2024-10-27 20:11:43 -0700

    Commit message

---
+++ Suresh/Sureash-B/table/dim_patient.Table.sql
@@ -0,0 +1,10 @@
+
+CREATE TABLE purgo_ai_ls.dim_patient (
+	patientid INTEGER, 
+	patientname VARCHAR(100), 
+	dateofbirth DATE, 
+	gender VARCHAR(10), 
+	contactinformation VARCHAR(255), 
+	medicalhistory VARCHAR(256)
+)
+
