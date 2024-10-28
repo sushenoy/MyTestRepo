f3f2c4a2fd69754a4fdfa53784f2e7523207c9b1
Purgo AI <support@purgo.ai>
2024-10-28 07:47:50 -0700

    Commit message

---
+++ Suresh/Suresh-B/table/dim_location.Table.sql
@@ -0,0 +1,10 @@
+
+CREATE TABLE purgo_ai_ls.dim_location (
+	locationid INTEGER, 
+	locationname VARCHAR(100), 
+	address VARCHAR(255), 
+	city VARCHAR(100), 
+	state VARCHAR(100), 
+	country VARCHAR(100)
+)
+
